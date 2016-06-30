dfc274dcd77a9aea588731ca99e1064bfb0a1438
Hans Vader <hans.vader@live.de>
2016-06-30 14:36:51 +0200

    Merge Fehler behoben (Nightbutton geht wieder)
    - Sorry

--- scripts/nightmode.js
+++ scripts/nightmode.js
@@ -4,3 +4,25 @@ if(localStorage.getItem("smpchtnightmode")==1)
 }else{
     document.getElementById("mystyle").href="css/loginDay.css";
 }
+
+$(document).ready
+(
+    function()
+    {
+        $("div.smpchtnightmode").on 
+        ( 
+          "click", 
+          function() 
+          { 
+            if(localStorage.getItem("smpchtnightmode")==1)
+            { 
+              localStorage.setItem("smpchtnightmode", "0");         
+              document.getElementById("mystyle").href="css/loginDay.css"; 
+            }else{ 
+              localStorage.setItem("smpchtnightmode", "1"); 
+              document.getElementById("mystyle").href="css/loginNightly.css"; 
+            } 
+          } 
+        ); 
+    }
+);
