8cb80e71ad37e59b9291601f1d4a60c23894b2aa
Hans Vader <hans-vader@live.de>
2016-07-06 09:27:50 +0200

    Merge Konflikt behoben

--- chat_content.php
+++ chat_content.php
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 <!--Hier wird der Chatinhalt gespeichert.-->
 <?php include "session.php"; ?>                                                 <!--Verhindert Zugriff ohne eingeloggt zu sein.-->
+=======
+<?php include 'session.php'?>
+>>>>>>> Stashed changes
 <tr></td><td>&nbsp;02:28:38&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>!</td></tr>
 <tr></td><td>&nbsp;02:28:38&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>"</td></tr>
 <tr></td><td>&nbsp;02:28:39&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>§</td></tr>
@@ -19,6 +23,7 @@
 <tr></td><td>&nbsp;04:51:20&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>sadf</td></tr>
 <tr></td><td>&nbsp;04:54:05&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>asdf</td></tr>
 <tr></td><td>&nbsp;04:54:24&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>asdf</td></tr>
+<<<<<<< Updated upstream
 <tr></td><td>&nbsp;08:15:03&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>1</td></tr>
 <tr></td><td>&nbsp;08:15:03&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>1</td></tr>
 <tr></td><td>&nbsp;08:15:03&nbsp;</td><td id='chat_entry'><strong>Joel&nbsp;:</strong><td class='inhalt'>1</td></tr>
@@ -54,3 +59,22 @@
 <tr></td><td>&nbsp;08:18:14&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>Kappa</td></tr>
 <tr></td><td>&nbsp;08:18:21&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>kappa</td></tr>
 <tr></td><td>&nbsp;08:18:55&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'> <img class='emoticon' src='img/kappa.png'></td></tr>
+=======
+<tr></td><td>&nbsp;09:00:41&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>Test</td></tr>
+<tr></td><td>&nbsp;09:00:48&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'> kappa</td></tr>
+<tr></td><td>&nbsp;09:00:53&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'> Kappa</td></tr>
+<tr></td><td>&nbsp;09:00:56&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>Kappa</td></tr>
+<tr></td><td>&nbsp;09:00:59&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>kappa</td></tr>
+<tr></td><td>&nbsp;09:01:14&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>kappa</td></tr>
+<tr></td><td>&nbsp;09:01:17&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'> <img class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:01:21&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'> <img class='emoticon' src='img/kappa.png'> <img class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:01:28&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>wie gehts? <img class='emoticon' src='img/kappa.png'>? </td></tr>
+<tr></td><td>&nbsp;09:09:23&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>Das Emoticon <img class='emoticon' src='img/kappa.png'>ross sollte jetzt funzen <img class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:21:27&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>Das Emoticon<img class='emoticon' src='img/kappa.png'>ross sollte jetzt funktionieren<img class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:23:34&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>d <img class='emoticon' src='img/kappaross.png'> b <img class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:24:15&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>jetzt geht <img class='emoticon' src='img/kappaross.png'> und <img style='width:20px' class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:24:27&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>jetzt geht <img class='emoticon' src='img/kappaross.png'> und <img style='width:20px' class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:24:45&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>jetzt geht <img class='emoticon' src='img/kappaross.png'> und <img style='width:25px' class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:25:07&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>jetzt geht <img style='width:20px'class='emoticon' src='img/kappaross.png'> und <img style='width:20px' class='emoticon' src='img/kappa.png'></td></tr>
+<tr></td><td>&nbsp;09:25:20&nbsp;</td><td id='chat_entry'><strong>d&nbsp;:</strong><td class='inhalt'>jetzt geht <img style='width:24px'class='emoticon' src='img/kappaross.png'> und <img style='width:20px' class='emoticon' src='img/kappa.png'></td></tr>
+>>>>>>> Stashed changes
